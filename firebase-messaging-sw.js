b551cedde28ef0c5434a55c6c78a7af2625e2377
suryasinghr07-commits <suryasinghr07@gmail.com>
2026-08-16 19:06:39 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,6 +1,28 @@
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
 
+self.addEventListener('notificationclick', function(event) {
+  event.stopImmediatePropagation();
+  event.notification.close();
+  
+  // 🚀 DYNAMIC TARGET URL: Yeh khud-b-hud aapke current domain ya sub-folder (jaise /habit-app/) ko utha lega!
+  const targetUrl = self.registration.scope;
+  
+  event.waitUntil(
+    clients.matchAll({ type: 'window', includeUncontrolled: true }).then(function(clientList) {
+      for (let i = 0; i < clientList.length; i++) {
+        let client = clientList[i];
+        if (client.url.startsWith(targetUrl) && 'focus' in client) {
+          return client.focus();
+        }
+      }
+      if (clients.openWindow) {
+        return clients.openWindow(targetUrl);
+      }
+    })
+  );
+});
+
 const firebaseConfig = {
   apiKey: "AIzaSyDxwEF9HuPIoJqMGl0h_Ipu5HpNLWEuKFk",
   authDomain: "habit-tracker-app-937f3.firebaseapp.com",
@@ -9,7 +31,5 @@ const firebaseConfig = {
   messagingSenderId: "985225384755",
   appId: "1:985225384755:web:7755a3ffcf2795ba7c6446"
 };
-
 firebase.initializeApp(firebaseConfig);
-// Firebase ka default system ab click aur background messages khud perfectly sambhal lega!
 const messaging = firebase.messaging();
